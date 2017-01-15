dbbd47921c791e11a01344a3f4490265591790bd
Tom Lai <tomtclai@gmail.com>
2017-01-14 19:18:10 -0800

    Initial Commit

---
+++ My Version/Time Tracker/Time Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Tracker
+//
+//  Created by Tom Lai on 1/14/17.
+//  Copyright © 2017 Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ My Version/Time Tracker/Time TrackerTests/Time_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Time_TrackerTests.swift
+//  Time TrackerTests
+//
+//  Created by Tom Lai on 1/14/17.
+//  Copyright © 2017 Lai. All rights reserved.
+//
+
+import XCTest
+@testable import Time_Tracker
+
+class Time_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
